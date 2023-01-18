53aecad2d7f546e13bd75fd97bfbac42cde68e09
Lorenzo Billi <lorenzoaebilli@gmail.com>
2023-01-18 20:33:11 +0100

    Added Tabulate class code

--- src/main/java/com/github/tabulate4j/Tabulate.java
+++ src/main/java/com/github/tabulate4j/Tabulate.java
@@ -19,6 +19,175 @@
 
 package com.github.tabulate4j;
 
-public class Tabulate {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Pretty-print tabular data in Java.
+ * @author Lorenzo Billi
+ */
+public class Tabulate {
+	/**
+	 * List containing the columns of the data. Each columns itself is represented as a list.
+	 */
+	private List<List<String>> columns;
+	
+	/**
+	 * List containing the headers of the data.
+	 */
+	private List<String> headers;
+	
+	/**
+	 * List containing the columns length.
+	 */
+	private List<Integer> columnsLength;
+	
+	/**
+	 * Padding used to format the content of each column. Default value is 1.
+	 */
+	private int padding;
+	
+	/**
+	 * Width of the resulting formatted table.
+	 */
+	private int width;
+	
+	/**
+	 * Creates a new <code>Tabulator</code> with the default padding value of 1.
+	 */
+	public Tabulate() {
+		columns = new ArrayList<>();
+		columnsLength = new ArrayList<>();
+		headers = new ArrayList<>();
+		padding = 1;
+		width = 0;
+	}
+
+	/**
+	 * Creates a new <code>Tabulator</code> with the specified padding value.
+	 * @param padding Value of the padding. Must be greater or equal than 1.
+	 */
+	public Tabulate(int padding) {
+		this();
+		if (padding < 1) {
+			throw new IllegalArgumentException("Padding value cannot be less than 1");
+		}
+		this.padding = padding;
+	}
+	
+	/**
+	 * Adds a new data column to the <code>Tabulator</code>.
+	 * @param column Data column to be added.
+	 */
+	public void addColumn(List<String> column) {
+		int length = 0;
+		for (String entry : column) {
+			if (entry.length() > length) {
+				length = entry.length();
+			}
+		}
+		length += padding * 2;
+		columns.add(column);
+		columnsLength.add(length);
+	}
+	
+	/**
+	 * Adds a new data column with the specified header to the <code>Tabulator</code>.
+	 * @param column Data column to be added.
+	 * @param header Header of the data column that is being added.
+	 */
+	public void addColumn(List<String> column, String header) {
+		addColumn(column);
+		headers.add(header);
+		if (header.length() + (padding * 2) > columnsLength.get(columnsLength.size() - 1)) {
+			columnsLength.set(columnsLength.size() - 1, header.length() + (padding * 2));
+		}
+	}
+	
+	/**
+	 * Computes the length of the resulting table by summing all the content of the <code>columnsLength</code> list.
+	 */
+	private void computeLength() {
+		for (int length : columnsLength) {
+			width += length;
+		}
+		width++;
+	}
+	
+	/**
+	 * Gets an horizontal separator element of the correct length, depending on the table size.
+	 * @return A <code>String</code> containing the horizontal separator element.
+	 */
+	private String getHorizontalSeparator(String sepChar) {
+		String separator = "+";
+		for (int n = 0; n < width; n++) {
+			separator += sepChar;
+		}
+		separator += "+\n";
+		return separator;
+	}
+	
+	/**
+	 * Gets a <code>String</code> containing the required amount of spaces in order to create the required padding.
+	 * @return The <code>String</code> containing the padding.
+	 */
+	private String getPadding() {
+		String padding = "";
+		for (int n = 0; n < this.padding; n++) {
+			padding += " ";
+		}
+		return padding;
+	}
+	
+	/**
+	 * Gets the data as a formatted table.
+	 * @return A <code>String</code> object containing all the formatted data.
+	 */
+	public String format() {
+		
+		computeLength();
+		boolean hasHeader = false;
+		
+		String formatted = "";
+		if (columns.get(0).size() == 0) {
+			return formatted;
+		}
+		
+		String horizontalSeparator = getHorizontalSeparator("-");
+		String padding = getPadding();
+		
+		if (headers.size() == columns.size()) {
+			hasHeader = true;
+			formatted += getHorizontalSeparator("=");
+			for (int i = 0; i < headers.size(); i++) {
+				formatted += "|";
+				String content = padding + headers.get(i) + padding;
+				formatted += content;
+				int remaining = columnsLength.get(i) - content.length();
+				while (remaining > 0) {
+					formatted += " ";
+					remaining--;
+				}
+			}
+			formatted += "|\n";
+		}
+	
+		for (int i = 0; i < columns.get(0).size(); i++) {
+			formatted += hasHeader && i == 0 ? getHorizontalSeparator("=") : horizontalSeparator;
+			for (int j = 0; j < columns.size(); j++) {
+				formatted += "|";
+				String content = padding + columns.get(j).get(i) + padding;
+				formatted += content;
+				int remaining = columnsLength.get(j) - content.length();
+				while (remaining > 0) {
+					formatted += " ";
+					remaining--;
+				}
+			}
+			formatted += "|\n";
+		}
+		formatted += horizontalSeparator;
+		
+		return formatted;
+	}
 }
